11c41930a73d6313723303ae51aaf063cdcd9b03
harika655 <harikaharika655@gmail.com>
2026-04-05 08:30:55 +0530

    resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <div className={darkMode ? "dark" : ""}>
+      
       <div className="bg-pink-900 dark:bg-blue-900 text-black dark:text-white min-h-screen">
         <Dashboard />
       </div>
