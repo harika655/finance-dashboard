e6f305245f1c888ccb7d171eef320aad886c945b
harika655 <harikaharika655@gmail.com>
2026-05-07 15:52:20 +0530

    Remove GitHub workflow

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/',
-=======
-  
->>>>>>> 7c37583 (saving changes)
-
-})
+  base: "/finance-dashboard/",
+});
